e11a64da8a810597b252ec7c7c15730ae925a6da
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-01-12 14:13:27 -0600

    Changing color styles and positioning

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 function renderCoffee(coffee) {
     let html = `<div class="coffees d-flex justify-content-center align-items-center" style="background-repeat: no-repeat; width: 250px; height: 250px; background-image: url(${coffee.img});">`;
     // html += '<td>' + coffee.id + '</td>';
-    html += '<div class=""><h1 class="fs-2 text-white">' + coffee.name +  `</h1>` ;
-    html +=  `<p class="text-white">` + coffee.roast  + '</p></div>';
+    html += '<div class=""><h1 class="fs-3 text-white">' + coffee.name +  `</h1>` ;
+    html +=  `<p class="text-white text-center">` + coffee.roast  + '</p></div>';
     html += '</div>';
 
     return html;
